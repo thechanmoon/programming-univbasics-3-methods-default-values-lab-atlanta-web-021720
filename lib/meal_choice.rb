be954dfe48cec0ec1dbc855ff548a8a76d11b49c
CHANMOON PARK <thechanmoon@gmail.com>
2020-02-12 19:43:27 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -3,5 +3,6 @@
 
 def meal_choice(veg1, veg2, protein = "meat")
   puts "What a nutritious meal!"
-  puts "A plate of #{protein} with #{veg1} and #{veg2}." 
+  if protein == "meat"
+    puts "A plate of #{protein} with #{veg1} and #{veg2}." 
 end  
