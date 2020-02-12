b882088abbd06bd67fb8cc40632067e0ba9c4beb
CHANMOON PARK <thechanmoon@gmail.com>
2020-02-12 20:13:57 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -4,6 +4,6 @@
 def meal_choice(veg1, veg2, protein = "meat")
   puts "What a nutritious meal!"
   retVal = "A plate of #{protein} with #{veg1} and #{veg2}." 
-  p retVal 
+  puts retVal 
   return retVal
 end  
